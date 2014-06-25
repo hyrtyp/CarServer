d637091544135774e366e41cf757f7a772603bc0
jiacunhe <jiacunhe@163.com>
2014-06-25 13:29:39 +0800

    增加紧急救援图片

--- CarServer/src/cn/com/hyrt/carserver/emergency/fragment/EmergencyFragment.java
+++ CarServer/src/cn/com/hyrt/carserver/emergency/fragment/EmergencyFragment.java
@@ -1,21 +1,35 @@
 package cn.com.hyrt.carserver.emergency.fragment;
 
 import cn.com.hyrt.carserver.R;
+import cn.com.hyrt.carserver.base.adapter.PortalGridAdapter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 public class EmergencyFragment extends Fragment{
 
 	private View rootView;
+	private GridView gvMyInfo;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.fragment_emergency, null);
+		findView();
+		int[] imgArray = new int[]{R.drawable.emergency_bxlp, R.drawable.emergency_jtbj,
+				R.drawable.emergency_ksqz, R.drawable.emergency_yjjy};
+		int[] textSourceArray = new int[]{R.string.emergency_bxlp, R.string.emergency_jtbj,
+				R.string.emergency_ksqz, R.string.emergency_yjjy};
+		PortalGridAdapter mAdapter = new PortalGridAdapter(imgArray, textSourceArray, getActivity());
+		gvMyInfo.setAdapter(mAdapter);
 		return rootView;
-		//test
 	}
+	
+	private void findView(){
+		gvMyInfo = (GridView) rootView.findViewById(R.id.gv_myInfo);
+	}
+
 }
