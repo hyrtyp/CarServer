d26d121e0efd9caca49ba830b69b0dd68bd0a894
zrong0 <zoerong916@gmail.com>
2014-06-25 09:38:18 +0800

    1.添加 我的信息布局

---
+++ CarServer/src/cn/com/hyrt/carserver/base/adapter/PortalGridAdapter.java
@@ -0,0 +1,59 @@
+package cn.com.hyrt.carserver.base.adapter;
+
+import cn.com.hyrt.carserver.R;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PortalGridAdapter extends BaseAdapter{
+
+	private int[] imgArray;
+	private int[] textSourceArray;
+	private Context mContext;
+	
+	
+	
+	public PortalGridAdapter(int[] imgArray, int[] textSourceArray,
+			Context mContext) {
+		super();
+		this.imgArray = imgArray;
+		this.textSourceArray = textSourceArray;
+		this.mContext = mContext;
+	}
+
+	@Override
+	public int getCount() {
+		return imgArray.length;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return arg0;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		return arg0;
+	}
+
+	@Override
+	public View getView(int index, View convertView, ViewGroup arg2) {
+		if(convertView == null){
+			LayoutInflater mInflater = LayoutInflater.from(mContext);
+			convertView = mInflater.inflate(R.layout.layout_portal_item, null);
+		}
+		
+		ImageView img = (ImageView) convertView.findViewById(R.id.iv_portal_img);
+		TextView text = (TextView) convertView.findViewById(R.id.tv_portal_text);
+		
+		img.setImageResource(imgArray[index]);
+		text.setText(textSourceArray[index]);
+		
+		return convertView;
+	}
+
+}
