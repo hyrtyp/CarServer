520e42876ff828b5f828e88d014d307116ef5109
zrong0 <zoerong916@gmail.com>
2014-06-25 17:17:22 +0800

    merge

--- CarServer/src/cn/com/hyrt/carserver/emergency/fragment/EmergencyFragment.java
+++ CarServer/src/cn/com/hyrt/carserver/emergency/fragment/EmergencyFragment.java
@@ -2,12 +2,18 @@ package cn.com.hyrt.carserver.emergency.fragment;
 
 import cn.com.hyrt.carserver.R;
 import cn.com.hyrt.carserver.base.adapter.PortalGridAdapter;
+import cn.com.hyrt.carserver.base.helper.LogHelper;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class EmergencyFragment extends Fragment{
 
@@ -23,8 +29,40 @@ public class EmergencyFragment extends Fragment{
 		int[] textSourceArray = new int[]{R.string.emergency_ksqz, R.string.emergency_yjjy,R.string.emergency_bxlp, R.string.emergency_jtbj};
 		PortalGridAdapter mAdapter = new PortalGridAdapter(imgArray, textSourceArray, getActivity());
 		gvMyInfo.setAdapter(mAdapter);
+		gvMyInfo.setOnItemClickListener(mOnItemClickListener);
 		return rootView;
 	}
+	
+	private AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
+   
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+				long arg3) {
+			LogHelper.i("tag", "position:"+position);
+			switch (position) {
+			case 0:
+				//快速求助
+				break;
+			case 1:
+				//一键救援
+				Intent intent=new Intent(Intent.ACTION_CALL,Uri.parse("tel:"+getString(R.string.emergencyyjjy))); 
+                //开始处理意图        执行 
+				EmergencyFragment.this.startActivity(intent);
+				break;
+			case 2:
+				//保险理赔
+				break;
+			case 3:
+				//交通报警
+				intent=new Intent(Intent.ACTION_CALL,Uri.parse("tel:"+getString(R.string.emergencyjtbj))); 
+                //开始处理意图        执行 
+				EmergencyFragment.this.startActivity(intent);
+				break;
+			default:
+				break;
+			}
+		}
+	};
 	
 	private void findView(){
 		gvMyInfo = (GridView) rootView.findViewById(R.id.gv_emergency);
