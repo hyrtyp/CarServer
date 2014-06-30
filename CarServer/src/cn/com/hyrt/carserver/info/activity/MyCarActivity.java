c49dd8976141c139e9a940f9b94aa9a3b36b6412
zrong0 <zoerong916@gmail.com>
2014-06-30 14:09:02 +0800

    1.添加 我的车辆

---
+++ CarServer/src/cn/com/hyrt/carserver/info/activity/MyCarActivity.java
@@ -0,0 +1,85 @@
+package cn.com.hyrt.carserver.info.activity;
+
+import net.tsz.afinal.annotation.view.ViewInject;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import cn.com.hyrt.carserver.R;
+import cn.com.hyrt.carserver.base.activity.BaseActivity;
+import cn.com.hyrt.carserver.base.baseFunction.Define;
+import cn.com.hyrt.carserver.base.baseFunction.Define.INFO_CAR_LIST;
+import cn.com.hyrt.carserver.base.helper.AlertHelper;
+import cn.com.hyrt.carserver.base.helper.LogHelper;
+import cn.com.hyrt.carserver.base.helper.WebServiceHelper;
+import cn.com.hyrt.carserver.base.view.FullListView;
+import cn.com.hyrt.carserver.base.view.PullToRefreshView;
+import cn.com.hyrt.carserver.info.adapter.MyCarAdapter;
+
+public class MyCarActivity extends BaseActivity{
+	
+	@ViewInject(id=R.id.lv_my_car) FullListView lvMyCar;
+	@ViewInject(id=R.id.tv_noData) TextView tvNoData;
+	@ViewInject(id=R.id.ptrv) PullToRefreshView ptrv;
+	
+	private MyCarAdapter mAdapter;
+	private Define.INFO_CAR_LIST cars;
+	private WebServiceHelper mWebServiceHelper;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_car);
+		ptrv.disableScroolUp();
+		setListener();
+		loadData();
+	}
+	
+	private void loadData(){
+		AlertHelper.getInstance(this).showLoading(getString(R.string.loading_msg));
+		if(mWebServiceHelper == null){
+			mWebServiceHelper = new WebServiceHelper(
+					new WebServiceHelper.RequestCallback<Define.INFO_CAR_LIST>() {
+
+						@Override
+						public void onSuccess(INFO_CAR_LIST result) {
+							ptrv.onHeaderRefreshComplete();
+							LogHelper.i("tag", "result:"+result.data.size());
+							AlertHelper.getInstance(MyCarActivity.this).hideLoading();
+							if(result == null || result.data.size() <= 0){
+								tvNoData.setVisibility(View.VISIBLE);
+								lvMyCar.setVisibility(View.GONE);
+							}else{
+								tvNoData.setVisibility(View.GONE);
+								lvMyCar.setVisibility(View.VISIBLE);
+							}
+							cars = result;
+							if(mAdapter == null){
+								mAdapter = new MyCarAdapter(cars, MyCarActivity.this);
+								lvMyCar.setAdapter(mAdapter);
+							}else{
+								mAdapter.notifyDataSetChanged();
+							}
+						}
+
+						@Override
+						public void onFailure(int errorNo, String errorMsg) {
+							ptrv.onHeaderRefreshComplete();
+							AlertHelper.getInstance(MyCarActivity.this).hideLoading();
+							tvNoData.setVisibility(View.VISIBLE);
+							lvMyCar.setVisibility(View.GONE);
+						}
+			}, this);
+		}
+		mWebServiceHelper.getTerminalCarList();
+	}
+	
+	private void setListener(){
+		ptrv.setOnHeaderRefreshListener(new PullToRefreshView.OnHeaderRefreshListener() {
+			
+			@Override
+			public void onHeaderRefresh(PullToRefreshView view) {
+				loadData();
+			}
+		});
+	}
+}
