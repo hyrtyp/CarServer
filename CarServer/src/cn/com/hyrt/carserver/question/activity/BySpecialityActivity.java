8ae46db373ec007332348c1fcd85cfc41506dd05
cqgsl <gaoshilin@hyrt.com.cn>
2014-07-06 22:06:33 +0800

    no

--- CarServer/src/cn/com/hyrt/carserver/question/activity/BySpecialityActivity.java
+++ CarServer/src/cn/com/hyrt/carserver/question/activity/BySpecialityActivity.java
@@ -1,14 +1,26 @@
 package cn.com.hyrt.carserver.question.activity;
 
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import cn.com.hyrt.carserver.R;
+import cn.com.hyrt.carserver.R.layout;
 import cn.com.hyrt.carserver.base.activity.BaseActivity;
+import cn.com.hyrt.carserver.base.baseFunction.ClassifyJsonParser;
+import cn.com.hyrt.carserver.base.helper.AlertHelper;
+import cn.com.hyrt.carserver.base.helper.WebServiceHelper;
 
 /**
  * 按专长找
@@ -19,59 +31,228 @@ public class BySpecialityActivity extends BaseActivity {
 
 	private ListView lst;
 	private myAdapter ma, ma1;
-
+	private Context context;
+	private LinearLayout layout;
+	private String title;
+	
+	private String [] names = null;//左边数组菜单
+	private String [] ids = null;//左边数组菜单
+	private ListView leftls, rightls;
+	
+	private WebServiceHelper mWebServiceHelper;
+	private List<Map<String, String>> oneList = null;
+	private List<List<Map<String, String>>> twoList = null;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		setContentView(R.layout.layout_question_insurance);
-		lst = (ListView) findViewById(R.id.lv_Strategy);
-		ma = new myAdapter(text1);
-		lst.setAdapter(ma);
+		setContentView(R.layout.layout_question_classification);
+		
+		
+		Intent intent = getIntent();
+		title = intent.getStringExtra("title");
+		setTitle(title);
+		
+		leftls = (ListView) findViewById(R.id.list_left);
+		rightls = (ListView) findViewById(R.id.list_right);
+		layout = (LinearLayout) findViewById(R.id.top_ll);
+		layout.setVisibility(View.GONE);
 
+		context=BySpecialityActivity.this;
+		leftLoadData();
 	}
 
-	private String[] text1 = { "投保1", "投保2", "投保3", "投保4", "投保5", "投保6", "投保7",
-			"投保8" };
-	private String[] text2 = { "理赔1", "理赔2", "理赔3", "理赔4", "理赔5", "理赔6", "理赔7",
-			"理赔8" };
+//	public void onAction(View v) 
+//	{
+//		switch (v.getId())
+//		{
+//			case R.id.btn1:
+//				ma = new myAdapter(text1);
+//				lst.setAdapter(ma);
+//				findViewById(R.id.btn1).setBackgroundColor(
+//						this.getResources().getColor(R.color.white));
+//				findViewById(R.id.btn2).setBackgroundColor(
+//						this.getResources().getColor(R.color.blue));
+//				((TextView) findViewById(R.id.text1)).setTextColor(this
+//						.getResources().getColor(R.color.black));
+//				((TextView) findViewById(R.id.text2)).setTextColor(this
+//						.getResources().getColor(R.color.white));
+//	
+//				break;
+//	
+//			case R.id.btn2:
+//				ma1 = new myAdapter(text2);
+//				lst.setAdapter(ma1);
+//				findViewById(R.id.btn2).setBackgroundColor(
+//						this.getResources().getColor(R.color.white));
+//				findViewById(R.id.btn1).setBackgroundColor(
+//						this.getResources().getColor(R.color.blue));
+//				((TextView) findViewById(R.id.text1)).setTextColor(this
+//						.getResources().getColor(R.color.white));
+//				((TextView) findViewById(R.id.text2)).setTextColor(this
+//						.getResources().getColor(R.color.black));
+//				break;
+//		}
+//	}
+	
+	private void leftLoadData() 
+	{
+		AlertHelper.getInstance(this).showLoading(getString(R.string.loading_msg));
+		if (mWebServiceHelper == null) 
+		{
+			mWebServiceHelper = new WebServiceHelper(new WebServiceHelper.OnSuccessListener()
+			{
+				@Override
+				public void onSuccess(String result)
+				{
+					AlertHelper.getInstance(context).hideLoading();
+					ClassifyJsonParser classifyJsonParser = new ClassifyJsonParser();
+					classifyJsonParser.parse(result);
+					
+					//一级分类
+					oneList =classifyJsonParser.getOneList();
+					if(oneList!=null && oneList.size()>0)
+					{
+						names=new String[oneList.size()];
+						int i=0;
+						for(Map<String, String> onemap : oneList)
+						{
+							names[i++]=onemap.get("name");
+						}
+						leftls.setAdapter(new leftAdapter(names,context));
+					}
+					
+					//二级分类
+					twoList = classifyJsonParser.getTwoList();	 
+				}
+			} , this);
+		}
+		
+		mWebServiceHelper.getMaintainFL("000020");
+	}
+	
+	private class leftAdapter extends BaseAdapter 
+	{
+		public String[] str;
+		public Context context;
+		private leftAdapter(String[] str,Context context) 
+		{
+			this.str = str;
+			this.context = context;
+		}
 
-	public void onAction(View v) {
-		switch (v.getId()) {
-		case R.id.btn1:
-			ma = new myAdapter(text1);
-			lst.setAdapter(ma);
-			findViewById(R.id.btn1).setBackgroundColor(
-					this.getResources().getColor(R.color.white));
-			findViewById(R.id.btn2).setBackgroundColor(
-					this.getResources().getColor(R.color.blue));
-			((TextView) findViewById(R.id.text1)).setTextColor(this
-					.getResources().getColor(R.color.black));
-			((TextView) findViewById(R.id.text2)).setTextColor(this
-					.getResources().getColor(R.color.white));
+		@Override
+		public int getCount() {
+			return str.length;
+		}
 
-			break;
+		@Override
+		public Object getItem(int position) {
+			return str[position];
+		}
 
-		case R.id.btn2:
-			ma1 = new myAdapter(text2);
-			lst.setAdapter(ma1);
-			findViewById(R.id.btn2).setBackgroundColor(
-					this.getResources().getColor(R.color.white));
-			findViewById(R.id.btn1).setBackgroundColor(
-					this.getResources().getColor(R.color.blue));
-			((TextView) findViewById(R.id.text1)).setTextColor(this
-					.getResources().getColor(R.color.white));
-			((TextView) findViewById(R.id.text2)).setTextColor(this
-					.getResources().getColor(R.color.black));
-			break;
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
 
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) 
+		{
+			convertView = View.inflate(BySpecialityActivity.this,
+					R.layout.question_maintain_left_item, null);
+			TextView text = (TextView) convertView.findViewById(R.id.text);
+			text.setText(str[position]);
+			
+			final LinearLayout llayout = (LinearLayout) convertView.findViewById(R.id.llayout);
+			llayout.setOnClickListener(new OnClickListener() 
+			{
+				@Override
+				public void onClick(View v)
+				{
+					String[] names = null;
+					String[] ids = null;
+					int j = 0;
+					if(twoList != null && !twoList.get(position).equals("[]") && twoList.get(position).size()>0)
+					{
+						List<Map<String, String>> list = twoList.get(position);
+						if(!list.equals("[]") && list.size()>0)
+						{
+							names = new String[list.size()];
+							ids = new String[list.size()];
+							for(Map<String, String> map :list)
+							{
+								names[j] = map.get("name");
+								ids[j]  = map.get("id");
+								j++;
+							}
+							rightls.setAdapter(new rightAdapter(names,ids,context));
+						}
+					}
+				}
+			});
+			return convertView;
 		}
 	}
 
-	private class myAdapter extends BaseAdapter {
+	private class rightAdapter extends BaseAdapter
+	{
+		public String[] str;
+		public String[] ids;
+		public Context context;
+		
+		private rightAdapter(String[] names,String[] ids,Context context) {
+			this.str = names;
+			this.ids = ids;
+			this.context = context;
+		}
 
+		@Override
+		public int getCount() {
+			return str.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return str[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			convertView = View.inflate(BySpecialityActivity.this,
+					R.layout.question_maintain_right_item, null);
+			final TextView text = (TextView) convertView.findViewById(R.id.text);
+			final TextView id = (TextView) convertView.findViewById(R.id.itemid);
+			
+			text.setText(str[position]);
+			id.setText(ids[position]);
+			id.setVisibility(View.GONE);
+			LinearLayout llayout = (LinearLayout) convertView.findViewById(R.id.llayout);
+			llayout.setOnClickListener(new OnClickListener() 
+			{
+				@Override
+				public void onClick(View v)
+				{
+					Intent ie=new Intent(context,CorrelationActivity.class);
+					ie.putExtra("name", str[position]);
+					ie.putExtra("id", ids[position]);
+					context.startActivity(ie);
+				}
+			});
+			return convertView;
+		}
+	}
+	
+	private class myAdapter extends BaseAdapter 
+	{
 		public String[] str;
 
 		private myAdapter(String[] str) {
@@ -80,37 +261,26 @@ public class BySpecialityActivity extends BaseActivity {
 
 		@Override
 		public int getCount() {
-			// TODO Auto-generated method stub
 			return str.length;
 		}
 
 		@Override
 		public Object getItem(int position) {
-			// TODO Auto-generated method stub
 			return str[position];
 		}
 
 		@Override
 		public long getItemId(int position) {
-			// TODO Auto-generated method stub
 			return position;
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			// TODO Auto-generated method stub
 			convertView = View.inflate(BySpecialityActivity.this,
 					R.layout.question_insurance_item, null);
 			TextView text = (TextView) convertView.findViewById(R.id.text);
 			text.setText(str[position]);
 			return convertView;
 		}
-
 	}
-
-	class ViewHolder {
-
-		public TextView text;
-	}
-
 }
--- CarServer/src/cn/com/hyrt/carserver/question/activity/CorrelationActivity.java
+++ CarServer/src/cn/com/hyrt/carserver/question/activity/CorrelationActivity.java
@@ -1,13 +1,22 @@
 package cn.com.hyrt.carserver.question.activity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.tsz.afinal.annotation.view.ViewInject;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ListView;
 import cn.com.hyrt.carserver.R;
-import cn.com.hyrt.carserver.R.color;
-import cn.com.hyrt.carserver.R.drawable;
 import cn.com.hyrt.carserver.base.activity.BaseActivity;
+import cn.com.hyrt.carserver.base.baseFunction.Define;
+import cn.com.hyrt.carserver.base.baseFunction.Define.QUESTION_CORRELATION;
+import cn.com.hyrt.carserver.base.helper.AlertHelper;
+import cn.com.hyrt.carserver.base.helper.WebServiceHelper;
+import cn.com.hyrt.carserver.base.view.PullToRefreshView;
+import cn.com.hyrt.carserver.question.adapter.CorrelationAdapter;
 
 /**
  * 相关问题
@@ -17,19 +26,133 @@ import cn.com.hyrt.carserver.base.activity.BaseActivity;
  */
 public class CorrelationActivity extends BaseActivity {
 
-	private Button leftBtn, rightBtn;
-
+	@ViewInject(id=R.id.ptrv) PullToRefreshView ptrv;
+	
+	String id;
+	String name;
+	Context context;
+	
+	ListView ls_correlation;
+	private CorrelationAdapter  correlationAdapter;
+	private WebServiceHelper mWebServiceHelper;
+	private List<Map<String, Object>> data = new ArrayList<Map<String,Object>>();
+	
+	private int pageNo = 1;
+	private boolean isLoadMore = false;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_question_correlation);
+		context = CorrelationActivity.this;
 
-		initView();
+		ls_correlation = (ListView) findViewById(R.id.ls_correlation);
+		id = getIntent().getExtras().getString("id");
+		name = getIntent().getExtras().getString("name");
+		AlertHelper.getInstance(this).showLoading(null);
+		setListener();
+		ptrv.disableScroolUp();
+		loadData();
 	}
-
-	private void initView() {
-
+	
+	private void setListener()
+	{
+		ptrv.setOnHeaderRefreshListener(new PullToRefreshView.OnHeaderRefreshListener()
+		{
+			@Override
+			public void onHeaderRefresh(PullToRefreshView view) 
+			{
+				isLoadMore = false;
+				loadData();
+			}
+		});
 		
+		ptrv.setOnFooterRefreshListener(new PullToRefreshView.OnFooterRefreshListener() {
+			
+			@Override
+			public void onFooterRefresh(PullToRefreshView view) {
+				isLoadMore = true;
+				loadData();
+			}
+		});
+	}
+	
+	private void loadData()
+	{
+		WebServiceHelper mCarInfoServiceHelper = new WebServiceHelper(
+			new WebServiceHelper.RequestCallback<Define.QUESTION_CORRELATION>() 
+			{
+				@SuppressWarnings("unused")
+				@Override
+				public void onSuccess(QUESTION_CORRELATION result)
+				{
+					AlertHelper.getInstance(CorrelationActivity.this).hideLoading();
+					if(result == null){
+						AlertHelper.getInstance(CorrelationActivity.this).showCenterToast(R.string.info_load_fail);
+						setResult(Define.RESULT_FROM_ALTER_CAR);
+						finish();
+					}
+					else
+					{
+						if(result == null && data.size()>0){
+							AlertHelper.getInstance(CorrelationActivity.this).showCenterToast(R.string.load_done);
+						}
+						
+						if(!isLoadMore)
+						{
+							data.clear();
+							ptrv.onHeaderRefreshComplete();
+						}
+						else
+						{
+							ptrv.onFooterRefreshComplete();
+						}
+						
+						isLoadMore = false;
+						
+						data.clear();
+						for(int i=0,j=result.data.size(); i<j; i++){
+							Map<String, Object> map = new HashMap<String, Object>();
+							map.put("content", result.data.get(i).content);
+							map.put("zyname", result.data.get(i).zyname);
+							map.put("username", result.data.get(i).username);
+							map.put("seekdate", result.data.get(i).seekdate);
+							map.put("userid", result.data.get(i).userid);
+							map.put("seektype", result.data.get(i).seektype);
+							map.put("attacpathname", result.data.get(i).attacpathname);
+							map.put("terminalid", result.data.get(i).terminalid);
+							map.put("attacpath", result.data.get(i).attacpath);
+							map.put("id", result.data.get(i).id);
+							data.add(map);
+						}
+
+						if(correlationAdapter == null){
+							correlationAdapter = new CorrelationAdapter(context,data);
+							ls_correlation.setAdapter(correlationAdapter);;
+						}else{
+							correlationAdapter.notifyDataSetChanged();
+						}
+					}
+				}
+
+				@Override
+				public void onFailure(int errorNo, String errorMsg)
+				{
+					AlertHelper.getInstance(CorrelationActivity.this).hideLoading();
+					AlertHelper.getInstance(CorrelationActivity.this).showCenterToast(R.string.info_load_fail);
+					setResult(Define.RESULT_FROM_ALTER_CAR);
+					ptrv.onFooterRefreshComplete();
+					finish();
+				}
+		}, this);
+		
+		if(isLoadMore)
+		{
+			pageNo++;
+		}else{
+			pageNo = 1;
+		}
+		mCarInfoServiceHelper.getCorrelation(id,pageNo);
 	}
 }
