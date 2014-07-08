07ffb2326c1e64c1676537d6fdd9a0e121066d10
cqgsl <gaoshilin@hyrt.com.cn>
2014-07-08 19:49:05 +0800

    finish

--- CarServer/src/cn/com/hyrt/carserver/question/adapter/QuestionBannerAdapter.java
+++ CarServer/src/cn/com/hyrt/carserver/question/adapter/QuestionBannerAdapter.java
@@ -2,52 +2,55 @@ package cn.com.hyrt.carserver.question.adapter;
 
 import java.util.List;
 
-import cn.com.hyrt.carserver.base.view.ImageLoaderView;
+import cn.com.hyrt.carserver.R;
 
 import android.support.v4.view.PagerAdapter;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class QuestionBannerAdapter extends PagerAdapter{
-
+public class QuestionBannerAdapter extends PagerAdapter
+{
 	private List<View> views;
 	
-	
-	public QuestionBannerAdapter(List<View> views) {
+	public QuestionBannerAdapter(List<View> views)
+	{
 		super();
 		this.views = views;
 	}
 
 	@Override
-	public int getCount() {
+	public int getCount()
+	{
 		return views.size();
 	}
 
 	@Override
-	public boolean isViewFromObject(View arg0, Object arg1) {
+	public boolean isViewFromObject(View arg0, Object arg1)
+	{
 		return arg0 == arg1;
 	}
 	
 	@Override
-	public void destroyItem(ViewGroup container, int position, Object object) {
-//		super.destroyItem(container, position, object);
+	public void destroyItem(ViewGroup container, int position, Object object)
+	{
 		container.removeView(views.get(position));
 	}
 	
 	@Override
-	public Object instantiateItem(ViewGroup container, int position) {
-		
+	public Object instantiateItem(final ViewGroup container, final int position)
+	{
 		container.addView(views.get(position), 0);
-		container.setOnClickListener(new OnClickListener() {
-			
+		container.setOnClickListener(new OnClickListener()
+		{
 			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+			public void onClick(View v)
+			{
+				TextView url = (TextView) container.findViewById(position).findViewById(R.id.tv_url);
 			}
 		});
 		return views.get(position);
 	}
-
 }
